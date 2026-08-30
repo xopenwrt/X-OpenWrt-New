9bc8ff1288cff299cca8d65cc96f209fda33f27d
xopenwrt <98641952+xopenwrt@users.noreply.github.com>
2026-08-30 22:27:13 +0800

    fix

--- Scripts/AutoBuild_Getenv.sh
+++ Scripts/AutoBuild_Getenv.sh
@@ -18,8 +18,7 @@ echo "------------------------------- IP Address Info --------------------------
 IP=`curl ip.115115.xyz -s`
 curl ip.115115.xyz -s
 # curl -s https://searchplugin.csdn.net/api/v1/ip/get?ip=${IP} | jq -r .data.address
-curl -s  "https://api.vore.top/api/IPdata?ip=${IP}"
-curl -s  "https://api.vore.top/api/IPdata?ip=${IP}" | jq -r .adcode.o
+# curl -s  "https://api.vore.top/api/IPdata?ip=${IP}" | jq -r .adcode.o
 }
 
 Get_Release_Info() {
